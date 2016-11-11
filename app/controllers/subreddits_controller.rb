0a8e8a77d513b1cd744e78a9087107af8f161ccd
Kojin Oshiba <kojin.oshiba@gmail.com>
2016-11-11 15:23:20 -0800

    create new method

--- app/controllers/subreddits_controller.rb
+++ app/controllers/subreddits_controller.rb
@@ -2,4 +2,20 @@ class SubredditsController < ApplicationController
   def index
     @subreddits = Subreddit.all
   end
+
+  def new
+    @subreddit = Subreddit.new
+  end
+
+  def create
+    @subreddit = Subreddit.new(subreddit_params)
+    redirect_to subreddits_index_path
+  end
+
+  private
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def subreddit_params
+    params.require(:subreddit).permit(:title)
+  end
 end
--- test/controllers/subreddits_controller_test.rb
+++ test/controllers/subreddits_controller_test.rb
@@ -1,9 +1,7 @@
 require 'test_helper'
 
 class SubredditsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get subreddits_index_url
-    assert_response :success
-  end
-
+  # test "the truth" do
+  #   assert true
+  # end
 end
